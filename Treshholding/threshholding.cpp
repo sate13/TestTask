3d7871b744c5c1f7c2b53bf596707a923d444def
Marc Satkowski <marc.satkowski@tu-dresden.de>
2014-06-13 22:58:22 +0200

    Erfolgreiche Implementierung der Schwellenwert ausgabe.

--- Treshholding/threshholding.cpp
+++ Treshholding/threshholding.cpp
@@ -4,9 +4,20 @@ ThreshHolding::ThreshHolding(QImage* image)
     : image(image) { }
 
 QImage ThreshHolding::changeThreshhold(const int& _a) {
-    // Neues OutputImage wird erstellt. Selbe Größe und Format wie das OriginalImage.
-    QImage _outputImage = image->alphaChannel();
+    QImage _outputImage = *image;
     actualThreshhold = _a;
+
+    for (int _w = 0; _w < image->width(); _w++) {
+        for (int _h = 0; _h < image->height(); _h++) {
+            int gray = qGray(image->pixel(_w, _h));
+            if(gray < actualThreshhold) {
+                _outputImage.setPixel(_w, _h, qRgb(0, 0, 0));
+            }
+            else {
+                _outputImage.setPixel(_w, _h, qRgb(255, 255, 255));
+            }
+        }
+    }
 
     return _outputImage;
 }
